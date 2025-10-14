d3fc068b33e974dc3034ec66aa7248db78156172
jayjain0901 <57716997+jayjain0901@users.noreply.github.com>
2025-10-14 14:28:46 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "rg-devops-k8s-demo"
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
+
+variable "cluster_name" {
+  description = "Name of the AKS cluster"
+  type        = string
+  default     = "aks-devops-demo"
+}
+
+variable "acr_name" {
+  description = "Name of the Azure Container Registry"
+  type        = string
+  default     = "acrdevopsk8sdemo"
+}
+
+variable "node_count" {
+  description = "Number of nodes in the default node pool"
+  type        = number
+  default     = 2
+}
+
+variable "node_vm_size" {
+  description = "VM size for the default node pool"
+  type        = string
+  default     = "Standard_B2s"
+}
